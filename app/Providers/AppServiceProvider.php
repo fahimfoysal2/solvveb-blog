22e82ea12a897c4e5e00f37e7bf45be6a5ea8ba1
Fahim Foysal <ffoysal6@gmail.com>
2021-05-09 02:26:21 +0600

    Key too Long Error while migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Key too long error
+        Schema::defaultStringLength(191);
+
         Paginator::useBootstrap();
 
         // demo data for sidebar
