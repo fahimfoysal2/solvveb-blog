8f57a8562d0d84a8c13820a31d0c3e9a6dd133c6
Fahim Foysal <ffoysal6@gmail.com>
2021-05-30 01:29:00 +0600

    key-too-long-error fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Key too long error
-        // Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191);
 
         Paginator::useBootstrap();
 
