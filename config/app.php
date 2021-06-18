66cf7aa8b924943d2109b27b9184177289c381b1
Fahim Foysal <ffoysal6@gmail.com>
2021-06-18 20:40:51 +0600

    1 in url

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'https://solvveb.herokuapp.com'),
 
-    'asset_url' => env('ASSET_URL', true),
+    'asset_url' => env('ASSET_URL', false),
 
     /*
     |--------------------------------------------------------------------------
